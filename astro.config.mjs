9e8731d75efaf77c4df6bcdcbdf0462fac1494c3
somossoluciones <somoscreandola@outlook.com>
2025-01-24 12:41:30 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://centroespiritual.com',
+  site: 'https://starsdog.co',
   integrations: [sitemap()],
   vite: {
     css: {
